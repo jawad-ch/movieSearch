d2012b12d5938885b9e1e8e2ca322360f76ca1e4
Your Name <you@example.com>
2020-12-09 14:41:10 +0000

    clean code 2

--- src/components/hooks/useHomeFetch.js
+++ src/components/hooks/useHomeFetch.js
@@ -1,7 +1,6 @@
 import { useState, useEffect, useCallback} from 'react'
 
-import { POPULAR_BASE_URL
-} from '../../config'
+import { POPULAR_BASE_URL } from '../../config'
 
 export const useHomeFetch = () => {
 
@@ -9,36 +8,37 @@ export const useHomeFetch = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
 
-    const fetchMovies = useCallback( (endpoint, type) => {
+    const fetchMovies = useCallback( async (endpoint, type) => {
         setError(false);
         setLoading(true);
     
         const isLoadMore = endpoint.search('page');
     
-          fetch(endpoint).then(response => response.json())
-          .then(data => {
-              setState({
-                ...state,
+        try {
+          const data = await (await fetch(endpoint)).json();
+          // .then(data => {
+              setState(prevState =>({
+                prevState,
                 movies:
                 isLoadMore !== -1
-                  ? [...state.movies, ...data.results]
+                  ? [prevState.movies, ...data.results]
                   : [...data.results],
-                heroImage: state.heroImage || data.results[0],
+                heroImage: prevState.heroImage || data.results[0],
                 currentPage: data.page,
                 totalPages: data.total_pages,
-              })
-
-          }).catch( (error) => {
-            setError(true);
-            console.log(error);
-          })
+              }))
+          
+        } catch (error) {
+          setError(true);
+          console.log(error);
+        }
         setLoading(false);
-      },[state]);
+      },[]);
     
       // Fetch popular movies initially on mount
       useEffect(() => {
           fetchMovies(POPULAR_BASE_URL, 'populaMovies');
-      }, []);
-    
+        // }
+      }, [fetchMovies]);
       return [{ state, loading, error }, fetchMovies];
     };
