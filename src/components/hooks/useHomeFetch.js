ab3399c89246311f5d9bcfd94bb8a631d29a8e88
Your Name <you@example.com>
2020-12-09 13:08:13 +0000

    clean code 1

--- src/components/hooks/useHomeFetch.js
+++ src/components/hooks/useHomeFetch.js
@@ -1,6 +1,7 @@
 import { useState, useEffect, useCallback} from 'react'
 
-import { POPULAR_BASE_URL } from '../../config'
+import { POPULAR_BASE_URL
+} from '../../config'
 
 export const useHomeFetch = () => {
 
@@ -8,13 +9,12 @@ export const useHomeFetch = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
 
-    const fetchMovies = useCallback((endpoint, type) => {
+    const fetchMovies = useCallback( (endpoint, type) => {
         setError(false);
         setLoading(true);
     
         const isLoadMore = endpoint.search('page');
     
-        try {
           fetch(endpoint).then(response => response.json())
           .then(data => {
               setState({
@@ -28,18 +28,17 @@ export const useHomeFetch = () => {
                 totalPages: data.total_pages,
               })
 
-          });
-          
-        } catch (error) {
-          setError(true);
-          console.log(error);
-        }
+          }).catch( (error) => {
+            setError(true);
+            console.log(error);
+          })
         setLoading(false);
       },[state]);
     
       // Fetch popular movies initially on mount
       useEffect(() => {
           fetchMovies(POPULAR_BASE_URL, 'populaMovies');
-      }, [fetchMovies]);
+      }, []);
+    
       return [{ state, loading, error }, fetchMovies];
     };
